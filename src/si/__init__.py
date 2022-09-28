153d24aaaca2840ee9eed7961f74caa95c491180
beatrizsilva16 <beatrizf.2000@gmail.com>
2022-09-28 10:15:02 +0100

    Adding my co-authorship to README.md file

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,6 +1,6 @@
-__author__ = "Fernando Cruz"
-__credits__ = ["Vítor Pereira, Fernando Cruz"]
+__author__ = "Beatriz Silva"
+__credits__ = ["Vítor Pereira, Fernando Cruz, Beatriz Silva"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "Fernando Cruz"
-__email__ = "fernando.cruz@ceb.uminho.pt"
+__maintainer__ = "Beatriz Silva"
+__email__ = "pg45465@alunos.uminho.pt"
