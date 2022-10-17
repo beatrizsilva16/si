eb14a0cdbf9a23187fb993b804ddad7de8b4f1bd
beatrizsilva16 <beatrizf.2000@gmail.com>
2022-10-17 11:50:47 +0100

    Update

---
+++ src/si/features_selection/variance_threshold.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+
+class VarianceThreshold:
+    def __int__(self, threshlod):
+        self.threshold = threshlod
+        self.variance = None #está vazio porque não vamos calcular variance aqui
+
+    def __init__(self, ):
+        variance = np.var(dataset)
+        self.variance = variance
+
+    return # o fit returna ele próprio
+
+    def transform (dataset, ) # vai retornar um novo dataset
+        X = dataset.X
+
+        mask =
+        novoX = dataset.X[:,mask]
+
+
+if __main__ == '__main__':
+    from si data dataset import Dataset
+    dataset = Dataset (X: np.array([[0, 2, 0, 1]
+                                    [0, 1, 4, 3]]
+                                    [0, 1, 1, 3]])
+
+
+
--- src/si/io/csv.py
+++ src/si/io/csv.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 from si.data.dataset import Dataset
 
-def read_csv(filename, sep=',', features=False, label=False):
+def read_csv(filename, str,
+             sep str
+             sep=',', features=False, label=False):
     """
     Parametros: filename, nome do ficheiro ou caminho;
                 sep,separador entre valores (default: ',');
@@ -12,3 +14,16 @@ def read_csv(filename, sep=',', features=False, label=False):
     Returns: pandas dataframe
     """
 
+    data = pd read_csv(filename, sep= sep)
+    if features and label:
+        features = data coluna [:1]
+        label = data coluna [:1]
+        x = data iloc [1, 0:-1]
+
+
+    data = pd.DataFrame(dataset.x)
+    if features:
+        data.coluna = dataset.features
+
+    if label:
+        data[]
