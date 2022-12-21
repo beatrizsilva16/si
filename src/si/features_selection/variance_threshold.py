380eb5145f54c4aca53a49f2d9a8229a4cde637e
beatrizsilva16 <beatrizf.2000@gmail.com>
2022-12-21 17:25:53 +0000

    Update_VarianceThreshold

--- src/si/features_selection/variance_threshold.py
+++ src/si/features_selection/variance_threshold.py
@@ -1,29 +1,76 @@
-import numpy as np
-import pandas as pd
+from src.si.data.dataset import Dataset
 
 class VarianceThreshold:
-    def __int__(self, threshlod):
+
+    """
+     Parameters
+    ----------
+    threshold : float, default=0
+        Features with a training-set variance lower than this threshold will
+        be removed. The default is to keep all features with non-zero variance,
+        i.e. remove the features that have the same value in all samples.
+    """
+
+    def __int__(self, threshlod:float):
         self.threshold = threshlod
         self.variance = None #está vazio porque não vamos calcular variance aqui
 
-    def __init__(self, ):
-        variance = np.var(dataset)
-        self.variance = variance
 
-    return # o fit returna ele próprio
 
-    def transform (dataset, ) # vai retornar um novo dataset
-        X = dataset.X
+    def fit(self, dataset: object):
 
-        mask =
-        novoX = dataset.X[:,mask]
+        """Learn empirical variances from X.
+         Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape (n_samples, n_features)
+            Data from which to compute variances, where `n_samples` is
+            the number of samples and `n_features` is the number of features.
+        y : any, default=None
+            Ignored. This parameter exists only for compatibility with
+            sklearn.pipeline.Pipeline.
+        Returns
+        -------
+        self : object
+            Returns the instance itself."""
+
+        self.variance = dataset.get_var()
+        return self # o fit returna ele próprio
+
+
+
+    def transform (self, dataset: object) -> object: # vai retornar um novo dataset
+
+        mask = self.variance > self.thresh
+        newX = dataset.X[:,mask]
+
+
+        if not (dataset.features is None):
+            dataset.features = [elem for ix,elem in enumerate(dataset.features) if mask[ix]]
+
+            print (dataset.features)
+
+        return Dataset(newX, dataset.y, dataset.features, dataset.label)
+
+
+
+    def fit_transform(self, dataset:object) -> object:
+        model = self.fit(dataset)
+        return model.transform(dataset)
 
 
 if __main__ == '__main__':
-    from si data dataset import Dataset
-    dataset = Dataset (X: np.array([[0, 2, 0, 1]
-                                    [0, 1, 4, 3]]
-                                    [0, 1, 1, 3]])
+
+    import numpy as np
+
+    dataset = Dataset (X:np.array([[0, 2, 0, 1]
+                                    [0, 1, 4, 3]
+                                    [0, 1, 1, 3]
+                                    [0, 4, 0, 2])
+                      np.array([1,2,3,4]),
+                      ["1","2","3","4"], "5")
+
+    temp = VarianceThreshold(1)
+    print(temp.fit_transform(dataset))
 
 
 
