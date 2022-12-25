09c4af82c820e3d1c2f027f80dfb126920f63391
beatrizsilva16 <beatrizf.2000@gmail.com>
2022-12-25 23:34:29 +0000

    Add accuracy

--- src/si/metrics/accuracy.py
+++ src/si/metrics/accuracy.py
@@ -1,7 +1,12 @@
-
 import numpy as np
 
-def accuracy (y_true: np:arrays):
+def accurancy(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """
+    Computes and returns the accurancy score of the model on a given dataset
+    :param y_true: np.ndarray - The true values of the labels
+    :param y_pred: np.ndarray - the labels predicted by a classifier
+    :return:
+    """
 
-    
-    return np.sum(y_true== y_pred)/len(y_true)
+    return np.sum (y_true == y_pred) / len(y_true)
+
