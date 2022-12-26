90a30a3f1239b3492b2a3314b1f2ca4388c33c56
beatrizsilva16 <beatrizf.2000@gmail.com>
2022-12-26 21:14:00 +0000

    Update select_percentile

--- src/si/features_selection/select_percentile.py
+++ src/si/features_selection/select_percentile.py
@@ -1,49 +1,37 @@
 import numpy as np
-from data.dataset import Dataset
+from si.data.dataset import Dataset
 from statistics.f_classification import f_classification
+from typing import Callable
 
 class SelectPercentile():
     '''
     Select features according to a percentile of the highest scores
-    parameters: score_funccallable, default=f_classif
-                Function taking two arrays X and y, and returning a pair of arrays (scores, pvalues)
-                or a single array with scores. Default is f_classif (see below “See Also”).
-                The default function only works with classification tasks.
+    parameters: score_func: callable, default=f_classif
+                    Function taking two arrays X and y, and returning a pair of arrays (scores, pvalues)
+                    or a single array with scores. Default is f_classif (see below “See Also”).
+                    The default function only works with classification tasks.
 
-                percentileint, default=10
-                Percent of features to keep.
+                percentile: int, default=10
+                    Percent of features to keep.
     '''
     def __init__(self, score_func, percentile: int):
         self.score_func = score_func
         self.percentile = percentile
-        self.F = None
-        self.p = None
+        self.F_value = None
+        self.p_value = None
 
     def fit(self, dataset) -> self:
-        self.f_value, self.p_value = self.score_func(dataset)
+        self.f_value, self.p_value = self.score_func(dataset) # retorna os valores F e p
         return self
 
     def transform(self, dataset):
         value = len(dataset.features) #valor total de features no dataset
         mask = value * (percentile/100) #percentil dado em percentagem
-        idxs = np.argsort(self.F)[- mask:]
-        features = np.array(dataset.features)[idxs]  # vai selecionar as features utilizando os indxs
+        idxs = np.argsort(self.F_value)[- mask:]
+        features = np.array(dataset.features)[idxs]  # vai selecionar as features utilizando os indexs
         return Dataset(X=dataset.X[:, idxs], y=dataset.y, features=list(features), label=dataset.label)
 
 
     def fit_transform(self, dataset):
         self.fit(dataset)
         return self.transform(dataset)
-
-    if __name__ == '__main__':
-        dataset = Dataset(X=np.array([[0, 2, 0, 3],
-                                      [0, 1, 4, 3],
-                                      [0, 1, 1, 3]]),
-                          y=np.array([0, 1, 0]),
-                          features=["f1", "f2", "f3", "f4"],
-                          label="y")
-        select = SelectPercentile(f_classification,25)
-        # chamar o método f_classification para o cálculo e introduzir valor de k
-        select = select.fit(dataset)
-        dataset = select.transform(dataset)
-        print(dataset)
