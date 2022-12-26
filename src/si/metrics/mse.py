dc15fb7701e52d006e74387a80a3a0a6553d0e0e
beatrizsilva16 <beatrizf.2000@gmail.com>
2022-12-26 09:19:30 +0000

    Update knn_classifier

--- src/si/metrics/mse.py
+++ src/si/metrics/mse.py
@@ -1,3 +1,14 @@
+import numpy as np
 
-
+def mse(y_true: np.ndarray, y_pred: np.array) -> float:
+    """
+    Returns the man squared error of the model on the given dataset
+    :param y_true: np.ndarray
+        The true labels of the dataset
+    :param y_pred: np.ndarray
+        The predicted labels of the dataset
+    :return: mse: float
+        The mean squared error of the model
+    """
+    return np.sum((y_true - y_pred) ** 2) / (len(y_true) * 2)
 
