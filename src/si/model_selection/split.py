87dcbbecb10094118dd628906812c4882b5a10c5
beatrizsilva16 <beatrizf.2000@gmail.com>
2022-12-30 10:08:48 +0000

    Update Split

--- src/si/model_selection/split.py
+++ src/si/model_selection/split.py
@@ -1,8 +1,11 @@
+from itertools import permutations
 from typing import Tuple
-import numpy as np
+from typing import Callable
 from si.data.dataset import Dataset
+import numpy as np
 
-def train_test_split(dataset: Dataset, test_size: float = 0.2, random_state: int =42) -> Tuple[Dataset, Dataset]:
+
+def train_test_split(dataset: Dataset, test_size: float, random_state: int =42)
 
     """
     Método para dividir um dataset em dataset de treino e teste
@@ -13,16 +16,18 @@ def train_test_split(dataset: Dataset, test_size: float = 0.2, random_state: int
     """
 
     # Set the random state
+    data = dataset.shape()[0]
     np.random.seed(random_state) # Para não obter coisas aleatórias e poder reproduzir esta pipeline sempre que necessário com mesmo resultado
 
     # Test set size
+    teste_size =test_size
     n_samples = dataset.shape()[0]
 
     # Get number of samples in the test set
-    n_test = int(n_samples * test_size)
+    n_test = int(data * test_size)
 
     # Get the dataset permutations
-    permutations = np.random.permutations(n_samples)
+    permutations = np.random.permutation(data)
 
     # Get the samples in the test set
     test_idxs = permutations[:n_test]
@@ -31,9 +36,9 @@ def train_test_split(dataset: Dataset, test_size: float = 0.2, random_state: int
     train_idxs = permutations[n_test:]
 
     # Get the training and testing datasets
-    train = Dataset(dataset.x[train_idxs], dataset.y[train_idxs], features=dataset.features, label = dataset.label)
+    train = Dataset(dataset.X[train_idxs], dataset.y[train_idxs], features=dataset.features, label = dataset.label)
 
-    test = Dataset(dataset.x[test_idxs], dataset.y[test_idxs], features= dataset.features, label=dataset.label)
+    test = Dataset(dataset.X[test_idxs], dataset.y[test_idxs], features= dataset.features, label=dataset.label)
     return train, test
 
 
