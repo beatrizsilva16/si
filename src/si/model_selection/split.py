69f48923213d4f5112cb02e172ad87a78786d749
beatrizsilva16 <beatrizf.2000@gmail.com>
2022-12-31 22:33:44 +0000

    Update split

--- src/si/model_selection/split.py
+++ src/si/model_selection/split.py
@@ -1,4 +1,3 @@
-from random import random
 from typing import Tuple
 
 import numpy as np
@@ -6,7 +5,7 @@ import numpy as np
 from si.data.dataset import Dataset
 
 
-def train_test_split(dataset: Dataset, test_size: float = 0.3, random_state: int = 0) -> Tuple[Dataset, Dataset]:
+def train_test_split(dataset: Dataset, test_size: float = 0.2, random_state: int = 42) -> Tuple[Dataset, Dataset]:
     """
     Split the dataset into training and testing sets.
     :param dataset: The dataset to split.
@@ -14,28 +13,31 @@ def train_test_split(dataset: Dataset, test_size: float = 0.3, random_state: int
     :param random_state: The proportion of the dataset to include in the test split.
     :returns: Tuple[Dataset, Dataset]: The training and test splits.
     """
-    # Set the random seed
+    # set random state
     np.random.seed(random_state)
 
-    # test set size
+    # get dataset size
     n_samples = dataset.shape()[0]
-    split_div = int(n_samples * test_size)
+
+    # get number of samples in the test set
+    n_test = int(n_samples * test_size)
 
     # get the dataset permutations
     permutations = np.random.permutation(n_samples)
 
-    # get the test and train sets
-    test_idx = permutations[:split_div]
-    train_idx = permutations[split_div:]
+    # get samples in the test set
+    test_idxs = permutations[:n_test]  # data until the number of samples defined is reached
+
+    # get samples in the train set
+    train_idxs = permutations[n_test:]  # remaining data goes to the train set
 
     # get the training and testing datasets
-    train = Dataset(dataset.X[train_idx], dataset.y[train_idx], features_name=dataset.features_names,
-                    label_name=dataset.label_name)
-
-    test = Dataset(dataset.X[test_idx], dataset.y[test_idx], features_names=dataset.features_names,
-                   label_name=dataset.label_name)
+    train = Dataset(dataset.X[train_idxs], dataset.y[train_idxs], features=dataset.features, label=dataset.label)
+    test = Dataset(dataset.X[test_idxs], dataset.y[test_idxs], features=dataset.features, label=dataset.label)
 
     return train, test
+
+
 
 
 
