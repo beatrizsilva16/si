2dc914d2908f28d4bb8bfabce14d717fdbf4d090
beatrizsilva16 <beatrizf.2000@gmail.com>
2022-12-31 22:43:10 +0000

    Update knn_regressor

--- src/si/neighbors/knn_regressor.py
+++ src/si/neighbors/knn_regressor.py
@@ -7,7 +7,16 @@ from si.statistics.euclidean_distance import euclidean_distance
 
 
 class KNNRegressor:
-    def __init__(self, k: int, distance: euclidean_distance = euclidean_distance):
+    """
+        KNN Regressor - Implements the K-Nearest Neighbors regressor on a ML model  based on a similarity measure
+        (like euclidean distance).
+    """
+    def __init__(self, k: int = 1, distance: callable = euclidean_distance):
+        """
+        Initialize the KNN Regressor.
+            :param k: int, number of nearest neighbors to be used
+            :param distance: callable, distance function to use
+        """
         self.k = k
         self.distance = distance
         self.dataset = None
@@ -21,7 +30,7 @@ class KNNRegressor:
         self.dataset = dataset
         return self
 
-    def _get_closet_label(self, x: np.ndarray):
+    def _get_closet_label(self, sample: np.ndarray) -> np.ndarray:
         """
         Calculates the class with the highest frequency.
         :param x: Array of samples.
@@ -29,19 +38,16 @@ class KNNRegressor:
         """
 
         # Calculates the distance between the samples and the dataset
-        distances = self.distance(x, self.dataset.x)
+        distances = self.distance(sample, self.dataset.X)
 
         # Sort the distances and get indexes
-        knn = np.argsort(distances)[:self.k]  # get the first k indexes of the sorted distances array
-        knn_labels = self.dataset.y[knn]
+        label_indexs = np.argsort(distances)[:self.k]
 
-        # Computes the mean of the matching classes
-        match_class_mean = np.mean(knn_labels)
+        # Get the labels values of indexes obtained
+        labels_values = self.dataset.y[label_indexs]
 
-        # Sorts the classes with the highest mean
-        high_freq_class = np.argsort(match_class_mean)[:self.k]
-
-        return high_freq_class
+        # Compute the mean value and return it
+        return np.man(labels_values)
 
     def predict(self, dataset: Dataset) -> np.ndarray:
         """
