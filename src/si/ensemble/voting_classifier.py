5cee42dfab5b53fcf84286d4df48937c90a6e462
beatrizsilva16 <beatrizf.2000@gmail.com>
2023-01-01 02:37:53 +0000

    Update voting_classifier

--- src/si/ensemble/voting_classifier.py
+++ src/si/ensemble/voting_classifier.py
@@ -89,3 +89,30 @@ class VotingClassifier:
         """
         return accuracy(dataset.y, self.predict(dataset))
 
+
+if __name__ == '__main__':
+
+    # import dataset
+    from si.data.dataset1 import Dataset
+    from si.model_selection.split import train_test_split
+    from si.neighbors.knn_classifier import KNNClassifier
+    from si.linear_model.logistic_regression import LogisticRegression
+
+    # load and split the dataset
+    dataset_ = Dataset.from_random(600, 100, 2)
+    dataset_train, dataset_test = train_test_split(dataset_, test_size=0.2)
+
+    # initialize the KNN and Logistic classifier
+    knn = KNNClassifier(3)
+    lg = LogisticRegression(l2_penalty=1, alpha=0.001, max_iter=1000)
+
+    # initialize the Voting classifier
+    voting = VotingClassifier([knn, lg])
+
+    voting.fit(dataset_train)
+
+    # compute the score
+    score = voting.score(dataset_test)
+    print(f"Score: {score}")
+
+    print(voting.predict(dataset_test))
