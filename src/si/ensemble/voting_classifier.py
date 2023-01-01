e8c7df1f2a6c5df2147f164bb03c548e318bd5a4
beatrizsilva16 <beatrizf.2000@gmail.com>
2023-01-01 21:16:41 +0000

    update voting_classifier

--- src/si/ensemble/voting_classifier.py
+++ src/si/ensemble/voting_classifier.py
@@ -1,92 +1,57 @@
 import numpy as np
-
 from si.data.dataset import Dataset
 from si.metrics.accuracy import accuracy
 
 
 class VotingClassifier:
     """
-    Ensemble classifier that uses the majority vote to predict the class labels.
-    Parameters
-    ----------
-    models : array-like, shape = [n_models]
-        Different models for the ensemble.
-    Attributes
-    ----------
+    It implements an ensemble classifier model which uses the majority vote to predict the class labels.
     """
-    def __init__(self, models: list, weighted: bool = False):
+    def __init__(self, models):
         """
         Initialize the ensemble classifier.
-        Parameters
-        ----------
-        models: array-like, shape = [n_models]
-            Different models for the ensemble.
+        :param models: array-like, shape = [n_models], different models for the ensemble.
         """
-        # parameters
+        #parameter
         self.models = models
-        self.weighted = weighted
 
-    def fit(self, dataset: Dataset) -> 'VotingClassifier':
+    def fit(self, dataset: Dataset):
         """
-        Fit the models according to the given training data.
-        Parameters
-        ----------
-        dataset : Dataset
-            The training data.
-        Returns
-        -------
-        self : VotingClassifier
-            The fitted model.
+        It fits the models according to the given training data.
+        :param dataset: Dataset, training dataset
+        :return: self, fitted model
         """
         for model in self.models:
             model.fit(dataset)
-
         return self
-
-    def _get_majority_vote(self, predictions: np.ndarray) -> int:
-        """
-        Helper function which determines and returns the most common label in a set
-        of predictions.
-        Parameters
-        ----------
-        predictions: np.ndarray
-            An array consisting of the labels predicted for a given example
-        """
-        labels, counts = np.unique(predictions, return_counts=True)
-        return labels[np.argmax(counts)]
 
     def predict(self, dataset: Dataset) -> np.ndarray:
         """
-        Predicts and returns the output of the dataset. To do so, it uses voting to
-        combine the predictions of the models in <self.models>. If <self.weighted> is
-        set to True, model predictions are weighted according to the respective scores.
-        Note: assumes that all models use the same scoring metric.
-        Parameters
-        ----------
-        dataset: Dataset
-            A Dataset object (the dataset containing the examples to be labeled)
+        It predict class labels for samples in X.
+        :param dataset: Dataset, testing dataset
+        :return: array-like, shape = [n_samples], predicted class labels
         """
-        # array containing the outputs of each k models in k rows
-        predictions = np.array([model.predict(dataset) for model in self.models])
-        # weigh model predictions based on the respective scores
-        if self.weighted:
-            # get model scores
-            scores = [model.score(dataset) for model in self.models]
-            # scale scores so that min_score = 1
-            min_scr = min(scores)
-            scores_sc = np.array([round((1 / min_scr) * scr) for scr in scores])
-            # update predictions in order to account for the computed weights
-            predictions = np.repeat(predictions, repeats=scores_sc, axis=0)
-        # voting is performed col-wise so that outputs of different models are compared
-        return np.apply_along_axis(self._get_majority_vote, axis=0, arr=predictions)
+        #helper function
+        def _get_majority_vote(pred: np.ndarray) -> int:
+            """
+            It returns the majority vote of the given predictions
+            :param pred: np.ndarray, predictions to get the majority vote of
+            :return: int, the majority vote of the given predictions
+            """
+            # get the most common label
+            labels, counts = np.unique(pred, return_counts=True)
+            # return_counts if True returns the number of times each unique item appears in the array
+            return labels[np.argmax(counts)]
+            # argmax obtains the one label that has more counts
+
+        predictions = np.array([model.predict(dataset) for model in self.models]).transpose()
+        return np.apply_along_axis(_get_majority_vote, axis=1, arr=predictions)
 
     def score(self, dataset: Dataset) -> float:
         """
-        Computes and returns the accuracy score of the model on the dataset.
-        Parameters
-        ----------
-        dataset: Dataset
-            A Dataset object (the dataset to compute the accuracy on)
+        It returns the mean accuracy on the given test data and labels.
+        :param dataset: Dataset, test data
+        :return: float, mean accuracy obtained
         """
-        y_pred = self.predict(dataset)
-        return accuracy(dataset.y, y_pred)
+        return accuracy(dataset.y, self.predict(dataset))
+
