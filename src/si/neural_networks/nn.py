f98a8cc04ec369f5cd21a052a5c1dc1e7b72bd70
beatrizsilva16 <beatrizf.2000@gmail.com>
2023-01-01 16:43:07 +0000

    Update nn

--- src/si/neural_networks/nn.py
+++ src/si/neural_networks/nn.py
@@ -1,6 +1,6 @@
 import numpy as np
 from typing import Callable
-from si.data.dataset_module import Dataset
+from si.data.dataset import Dataset
 from si.metrics.accuracy import accuracy
 from si.metrics.mse import mse, mse_derivative
 
